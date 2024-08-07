3025c10528521530caa3bcda3dd68d0ed5868608
ThatOnePerson <87896375+ThatOnePers0n@users.noreply.github.com>
2024-08-07 16:29:57 -0700

    Upload site

---
+++ splash.js
@@ -0,0 +1,5 @@
+var text_location = document.getElementById("subtitle");
+var splash = new Array ();splash[0] = "*looks at sister* die.";
+splash[1] = "testing!";
+var i = Math.floor(2*Math.random());
+text_location.innerHTML = splash[i];
